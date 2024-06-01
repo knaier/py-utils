0914b36063820469d6427f1913e62990437a2aad
knaier <kieran.russell.smith@gmail.com>
2024-06-01 21:33:18 +0100

    lists

---
+++ lists_tuples.py
@@ -0,0 +1,8 @@
+# Tuples are immutable lists, using () instead of []
+dimensions = (10, 100)
+print(dimensions)
+print(dimensions[0])
+
+# Tuples with just 1 value are defined with a presence of a comma, not just ()
+dimensions = (10,)
+print(dimensions)
